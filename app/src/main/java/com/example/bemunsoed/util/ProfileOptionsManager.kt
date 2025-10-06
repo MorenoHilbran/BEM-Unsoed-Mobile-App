8ada07e1209a429d8bdb67c579218e40c5fd26b7
Moreno <moren.glenardi@gmail.com>
2025-10-06 13:41:05 +0700

    update

--- app/src/main/java/com/example/bemunsoed/util/ProfileOptionsManager.kt
+++ app/src/main/java/com/example/bemunsoed/util/ProfileOptionsManager.kt
@@ -7,42 +7,40 @@ object ProfileOptionsManager {
 
     fun getProfilePhotoOptions(): List<ProfileOption> {
         return listOf(
-            ProfileOption("Default", R.drawable.ic_profile_placeholder, "default"),
-            ProfileOption("Avatar 1", R.drawable.ic_person, "avatar1"),
-            ProfileOption("Avatar 2", R.drawable.ic_profile, "avatar2"),
-            ProfileOption("Avatar 3", R.drawable.profil, "avatar3"),
-            ProfileOption("Avatar 4", R.drawable.ic_comic, "avatar4"),
-            ProfileOption("Avatar 5", R.drawable.ic_forum, "avatar5")
+            ProfileOption("Default", R.drawable.ava_1, "default"),
+            ProfileOption("Avatar 1", R.drawable.ava_2, "avatar1"),
+            ProfileOption("Avatar 2", R.drawable.ava_3, "avatar2"),
+            ProfileOption("Avatar 3", R.drawable.ava_4, "avatar3"),
+            ProfileOption("Avatar 4", R.drawable.ava_5, "avatar4"),
         )
     }
 
     fun getBackgroundOptions(): List<ProfileOption> {
         return listOf(
             ProfileOption("Default", R.drawable.default_profile_bg, "default"),
-            ProfileOption("Blue Gradient", R.drawable.profile_background, "blue_gradient"),
-            ProfileOption("Green Pattern", R.drawable.banner_background, "green_pattern"),
-            ProfileOption("Orange Theme", R.drawable.button_primary, "orange_theme"),
-            ProfileOption("Purple Design", R.drawable.badge_background, "purple_design")
+            ProfileOption("Brave Pink", R.drawable.pink_background, "brave_pink"),
+            ProfileOption("Hero Green", R.drawable.green_background, "hero_green"),
+            ProfileOption("Orange Theme", R.drawable.orange_background, "orange_theme"),
+            ProfileOption("Purple Theme", R.drawable.purple_background, "purple_design")
         )
     }
 
     fun getProfilePhotoDrawable(photoId: String): Int {
         return when (photoId) {
-            "avatar1" -> R.drawable.ic_person
-            "avatar2" -> R.drawable.ic_profile
-            "avatar3" -> R.drawable.profil
-            "avatar4" -> R.drawable.ic_comic
-            "avatar5" -> R.drawable.ic_forum
-            else -> R.drawable.ic_profile_placeholder
+            "avatar1" -> R.drawable.ava_1
+            "avatar2" -> R.drawable.ava_2
+            "avatar3" -> R.drawable.ava_3
+            "avatar4" -> R.drawable.ava_4
+            else -> R.drawable.ava_5
         }
     }
 
     fun getBackgroundDrawable(backgroundId: String): Int {
         return when (backgroundId) {
-            "blue_gradient" -> R.drawable.profile_background
-            "green_pattern" -> R.drawable.banner_background
-            "orange_theme" -> R.drawable.button_primary
-            "purple_design" -> R.drawable.badge_background
+            "brave_pink" -> R.drawable.pink_background
+            "hero_green" -> R.drawable.green_background
+            "orange_theme" -> R.drawable.orange_background
+            "purple_design" -> R.drawable.purple_background
             else -> R.drawable.default_profile_bg
         }
     }
@@ -53,15 +51,14 @@ object ProfileOptionsManager {
             "avatar2" -> 2
             "avatar3" -> 3
             "avatar4" -> 4
-            "avatar5" -> 5
             else -> 0
         }
     }
 
     fun getBackgroundPosition(backgroundId: String): Int {
         return when (backgroundId) {
-            "blue_gradient" -> 1
-            "green_pattern" -> 2
+            "brave_pink" -> 1
+            "hero_green" -> 2
             "orange_theme" -> 3
             "purple_design" -> 4
             else -> 0
